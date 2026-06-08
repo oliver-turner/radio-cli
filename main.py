edc8705c1bd60458e346261b82c15be2ed89c0d8
oturner01 <oliver.turner1@students.mq.edu.au>
2026-06-08 22:54:47 +1000

    finding non talk music radio stations

--- main.py
+++ main.py
@@ -4,16 +4,11 @@ from pyradios import RadioBrowser
 
 rb = RadioBrowser()
 results = ""
-stations = ["bbc world service", "abc radio", "taiwan"]
+stations = ["fip", "Swiss Jazz", "somaFM groove", "Paradise main"]
 
-results = rb.search(hidebroken=True, countrycode="tw")
+for station in stations:
+    results = rb.search(name=station, hidebroken=True, order="votes")
 
-if results:
-    print(json.dumps(results, indent=2))
-
-# for station in stations:
-#    results = rb.search(name=station)
-#
-#    if results:
-#        print(json.dumps(results, indent=2))
-#        print("\n")
+    if results:
+        print(json.dumps(results, indent=2))
+        print("\n")
