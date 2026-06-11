2993ad1e6ad5adfddbb993fdea3cac100b52449d
oliver-turner <wanderingturner@gmail.com>
2026-06-11 10:38:27 +1000

    moved the print statemtns from main into menu.py, added first draft menu layout and terminal ui

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-from src.stations_json_parser import parse_json
+from src.menu import interactive_menu
 
 
 def main():
-    my_stations = parse_json()
-    print("=== My Stations ===\n")
-    for station in my_stations:
-        print(
-            f"Name: {station.name}\nCountry: {station.country}\nQuality: {station.codec} ({station.bitrate}Kpbs)\nURL: {station.url_resolved}\n"
-        )
+    interactive_menu()
 
 
 if __name__ == "__main__":
--- src/menu.py
+++ src/menu.py
@@ -1,2 +1,24 @@
 # Create the terminal interactive menu from here
 #
+import os
+
+from src.stations_json_parser import parse_json
+
+
+def interactive_menu():
+    tui_width = os.get_terminal_size().columns
+    boundary = "=" * tui_width
+
+    my_stations = parse_json()
+
+    print(boundary)
+    print(" My Stations ".center(tui_width, "="))
+    print()
+
+    for station in my_stations:
+        print(
+            f"Name: {station.name}\nCountry: {station.country}\n"
+            f"Quality: {station.codec} ({station.bitrate}Kpbs)\n"
+            f"URL: {station.url_resolved}\n"
+        )
+    print(boundary)
