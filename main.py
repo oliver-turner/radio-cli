6626322315e3e00346580321bcad6f52db062d23
oliver-turner <wanderingturner@gmail.com>
2026-06-12 18:32:44 +1000

    simple UI just so i can get it to work, created interactive welcome menu and choice logic using input, cleaned up the saved stations function to show_my_stations

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from src.menu import interactive_menu
+import sys
+
+from src.menu import show_my_stations, show_welcome_menu
 
 
 def main():
-    interactive_menu()
+    print("\nWelcome to Radio CLI")
+    while True:
+        show_welcome_menu()
+
+        choice = input("\nType the index and press enter :) ").strip()
+
+        if choice == "1":
+            print("\nDo this next")
+        elif choice == "2":
+            show_my_stations()
+        elif choice == "3":
+            print("\nGoodbye :)\n")
+            sys.exit()
+        else:
+            print("\nNot an option, please type either 1, 2 or 3")
 
 
 if __name__ == "__main__":
--- src/menu.py
+++ src/menu.py
@@ -1,24 +1,28 @@
 # Create the terminal interactive menu from here
-#
-import os
 
 from src.stations_json_parser import parse_json
 
 
-def interactive_menu():
-    tui_width = os.get_terminal_size().columns
-    boundary = "=" * tui_width
+def show_my_stations():
+
+    VISUAL_SEPARATOR = "=" * 100
 
     my_stations = parse_json()
-
-    print(boundary)
-    print(" My Stations ".center(tui_width, "="))
     print()
-
+    print(VISUAL_SEPARATOR)
+    print("My Saved Stations:\n")
     for station in my_stations:
         print(
             f"Name: {station.name}\nCountry: {station.country}\n"
             f"Quality: {station.codec} ({station.bitrate}Kpbs)\n"
             f"URL: {station.url_resolved}\n"
         )
-    print(boundary)
+    print(VISUAL_SEPARATOR)
+    return
+
+
+def show_welcome_menu():
+    print("\nMenu")
+    print("1. Play")
+    print("2. View Saved Stations")
+    print("3. Exit Program")
