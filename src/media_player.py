b9564d52eda5ab21b5bb2aa11af522f40d6c608a
oliver-turner <oliver01turner@gmail.com>
2026-06-19 14:30:27 +1000

    created media_player file, first pseudocode attempt

---
+++ src/media_player.py
@@ -0,0 +1,8 @@
+# Import pywin32
+# Class that connects to Windows Media Player
+# Follows the singleton design pattern
+# Create a Class Media_Player
+# Initialise object using constructor
+# Create connection to media player api
+# define functions for start, stop
+# Handle async play
