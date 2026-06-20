19acbe2acf86a36be6b49fba3a29ff6486224391
oliver-turner <oliver01turner@gmail.com>
2026-06-20 17:26:22 +1000

    MediaPlayer class completed, start and stop commands along with other helpers implemented

--- src/media_player.py
+++ src/media_player.py
@@ -1,33 +1,65 @@
-# Handle async play
-# Need to have name of station in windows + a system tray
-
 import asyncio
+import time
 
 from winrt.windows.foundation import Uri
-from winrt.windows.media.playback import MediaPlayer
-
-mp = MediaPlayer()
-
-def play_stream(url_to_play):
-    # Initialise media player
-    # parse url into a Uri
-    # play url
-
-    uri = Uri(url_to_play)
-    mp.set_uri_source(uri)
-    mp.play()
+from winrt.windows.media.playback import MediaPlaybackState, MediaPlayer
 
 
-play_stream(
-    "https://mediaserviceslive.akamaized.net/hls/live/2038316/classicfmnsw/index.m3u8"
-)
-
-input("\nPress ENTER to close the player and exit script...\n")
-
-
-# Create Media_Player class
-# Follows the singleton design pattern
-# Initialise object using constructor
+# Handle async play
+# Need to have name of station in windows + a system tray
 # define functions for start, stop, tracking functions for getting states for loading and active.
 # Graceful exit functionality
 class Media_Player:
+    STREAM_PLAYING = MediaPlaybackState.PLAYING.value
+    STREAM_PAUSED = MediaPlaybackState.PAUSED.value
+
+    def __init__(self):
+        self.radio = MediaPlayer()
+
+    def play_stream(self, url_to_play: str):
+
+        uri = Uri(url_to_play)
+        self.radio.set_uri_source(uri)
+        self.radio.play()
+
+    def stop(self):
+        if self.is_playing():
+            self.radio.pause()
+        else:
+            print("\nNothing is playing at the moment")
+
+    def start(self):
+        if self.is_paused():
+            self.radio.play()
+        else:
+            print("\nUnable to play right now :(")
+
+    def get_status(self):
+        return self.radio.playback_session.playback_state
+
+    def is_playing(self) -> bool:
+        return self.get_status() == self.STREAM_PLAYING
+
+    def is_paused(self) -> bool:
+        return self.get_status() == self.STREAM_PAUSED
+
+
+radio = Media_Player()
+
+radio.play_stream(
+    "https://mediaserviceslive.akamaized.net/hls/live/2038316/classicfmnsw/index.m3u8"
+)
+time.sleep(5)
+
+radio.stop()
+
+time.sleep(2)
+
+radio.stop()
+
+time.sleep(2)
+
+radio.start()
+
+
+input("\nPress ENTER to close the player and exit script...\n")
