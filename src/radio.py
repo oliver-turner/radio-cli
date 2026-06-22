477981c457fbf58fa13a5da32ae45362463fcf4b
oliver-turner <oliver01turner@gmail.com>
2026-06-22 15:07:43 +1000

    use the play radio function to return the active_station_name back up to main

--- src/radio.py
+++ src/radio.py
@@ -5,8 +5,8 @@ from src.menu import show_stations
 # Ask for the input, choice = the input
 # Use the choice to filter by stationuuid and use that to find the URL
 # Use mpv play URL
-def play_station(radio, user_list):
-    show_stations(user_list)
+def play_station(radio, user_station_list):
+    show_stations(user_station_list)
     while True:
         choice = input("Type the index and press enter [or q for exit]: ").strip()
 
@@ -17,17 +17,16 @@ def play_station(radio, user_list):
         elif choice.isdigit():
             choice_int = int(choice)
 
-            if 1 <= choice_int <= len(user_list):
-                selected_station = user_list[choice_int - 1]
+            if 1 <= choice_int <= len(user_station_list):
+                selected_station = user_station_list[choice_int - 1]
                 url = selected_station.url_resolved
-                name = selected_station.name
-                print(f"\nYou chose {name}\n")
+                active_station_name = selected_station.name
+                print(f"\nYou chose {active_station_name}\n")
                 radio.play_stream(url)
+                return active_station_name
 
             else:
                 print("That is not a station, please try again ")
 
         else:
             print("Please choose a valid number")
-
-def now_playing(radio):
