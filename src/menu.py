36f7c88e1668ceb1897b3d0b6bead27e7e6d0b21
oliver-turner <oliver01turner@gmail.com>
2026-06-22 15:07:43 +1000

    parse active_station_name and reset to current station playing

--- src/menu.py
+++ src/menu.py
@@ -1,12 +1,12 @@
-VISUAL_SEPARATOR = "=" * 100
+from src.constants import VISUAL_SEPARATOR
 
 
-def show_stations(user_list):
+def show_stations(user_station_list):
     print()
     print(VISUAL_SEPARATOR)
     print()
     print("My Saved Stations:\n")
-    for index, station in enumerate(user_list, start=1):
+    for index, station in enumerate(user_station_list, start=1):
         print(
             f"{index}: {station.name}\nCountry: {station.country}\n"
             f"Quality: {station.codec} ({station.bitrate}Kpbs)\n",
@@ -15,8 +15,9 @@ def show_stations(user_list):
     return
 
 
-def show_welcome():
+def show_welcome(active_station_name):
     print("\nMenu")
+    print(f"Now playing: {active_station_name}")
     print("1. Play")
     print("2. View Saved Stations")
     print("Enter q to exit")
