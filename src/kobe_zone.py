b15c6b85006fbc0ad6c29970f2df7f4e2be211c5
oliver-turner <oliver01turner@gmail.com>
2026-07-11 22:14:41 +1000

    kobe zone stub for development later

---
+++ src/kobe_zone.py
@@ -0,0 +1,10 @@
+from textual.widgets import Static
+from src.constants import ascii_kobes
+
+class KobeZone(Static):
+
+
+   def __init__(self, **kwargs) -> None:
+
+
+        super().__init__(self, **kwargs)
